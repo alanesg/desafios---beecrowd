1947fa055c3fe2e2e49ef4d418369e00deab6653
alanesg <alanesGsilva@gmail.com>
2024-10-24 15:19:55 -0300

    desafio 1005 concluido

---
+++ 1005/index.js
@@ -0,0 +1,22 @@
+const readline = require ('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question("Digite a primeira nota: " ,(entradaUsuario)=>{
+
+    var A = parseFloat(entradaUsuario)
+
+    rl.question("Digite a segunda nota: ",(entradaUsuarioDois)=>{
+
+        var B = parseFloat(entradaUsuarioDois)
+        var media = (A * 3.5 + B * 7.5)/ 11;
+    
+        console.log ("MEDIA = " + media.toFixed(5))
+
+        rl.close()
+    })
+
+})
